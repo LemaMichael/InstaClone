d8a29c99896fedb74c5883fd3ce21e99f6f6161f
Michael Lema <mel526@nyu.edu>
2018-10-06 10:35:35 -0400

    Firebase now saves user info and image

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -14,8 +14,16 @@ class ViewController: UIViewController {
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -41,11 +49,12 @@ class ViewController: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
+        tf.isSecureTextEntry = true
         tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return tf
     }()
     let signUpButton: UIButton = {
-       let button = UIButton(type: .system)
+        let button = UIButton(type: .system)
         button.setTitle("Sign Up", for: .normal)
         button.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         button.layer.cornerRadius = 5
@@ -56,7 +65,7 @@ class ViewController: UIViewController {
         return button
     }()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(plusPhotoButton)
@@ -74,7 +83,7 @@ class ViewController: UIViewController {
         
         stackView.anchor(top: plusPhotoButton.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingBottom: 0, paddingLeft: 40, paddingRight: 40, width: 0, height: 200)
     }
-
+    
     @objc func handleSignUp() {
         guard let email = emailTextField.text, email.count > 0 else { return }
         guard let username = usernameTextField.text, username.count > 0 else { return }
@@ -86,6 +95,39 @@ class ViewController: UIViewController {
                 return
             }
             print("Created User!:", authDataResult?.user.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            let filename = NSUUID().uuidString
+            
+            let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+            storageRef.putData(uploadData, metadata: nil, completion: { (metaData, error) in
+                if let error = error {
+                    print("Failed to upload profile image: ", error)
+                }
+                
+                // Retrieve downloadURL
+                storageRef.downloadURL(completion: { (downloadURL, error) in
+                    if let error = error {
+                        print("Failed to fetch downloadURL: ", error)
+                        return
+                    }
+                    guard let profileImageURL = downloadURL?.absoluteString else { return }
+                    print("Sucessfully uploaded profile image: ", profileImageURL)
+                    
+                    guard let uid = authDataResult?.user.uid else { return }
+                    
+                    let dictionaryValues = ["username": username, "profileImageURL": profileImageURL]
+                    let values = [uid: dictionaryValues]
+                    Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (error, reference) in
+                        if let error = error {
+                            print("failed to save user info into database: ", error)
+                            return
+                        }
+                        print("Successfully saved user info into database")
+                    })
+                })
+            })
         }
     }
     
@@ -105,3 +147,19 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
