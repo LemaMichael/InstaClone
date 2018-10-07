13b483796a576391ccfb00e94fddf9ba388ab9d9
Michael Lema <mel526@nyu.edu>
2018-10-07 17:43:41 -0400

    User can upload images and captions.

--- InstaClone/Add Photo/SharePhotoController.swift
+++ InstaClone/Add Photo/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
     
@@ -54,7 +55,55 @@ class SharePhotoController: UIViewController {
     }
     
     @objc func handleShare() {
+        guard let caption = textView.text, !caption.isEmpty else { return }
+        guard let image = selectedImage else { return }
+        guard let uploadData = image.jpegData(compressionQuality: 0.5) else { return }
+        navigationItem.rightBarButtonItem?.isEnabled = false
+
+        let fileName = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child("posts").child(fileName)
+        storageRef.putData(uploadData, metadata: nil) { (metadata, error) in
+            
+            if let error = error {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to upload post image: ", error)
+                return
+            }
+            
+            print("Successfully uploaded post image")
+            storageRef.downloadURL(completion: { (downloadURL, error) in
+                if let error = error {
+                    self.navigationItem.rightBarButtonItem?.isEnabled = true
+                    print("Failed to fetch downloadURL: ", error)
+                    return
+                }
+                guard let imageURL = downloadURL?.absoluteString else { return }
+                print("Succesfully uploaded post image: ", imageURL)
+                self.saveToDatabseWithImageUrl(imageURL: imageURL)
+            })
+            
+        }
+    }
+    
+    fileprivate func saveToDatabseWithImageUrl(imageURL: String) {
+        guard let postImage = selectedImage else { return }
+        guard let caption = textView.text else { return }
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let userPostRef =  Database.database().reference().child("posts").child(uid)
+        let ref = userPostRef.childByAutoId()
+        
+        let values = ["imageUrl": imageURL, "caption": caption, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String : Any]
+        
+        ref.updateChildValues(values) { (error, databaseReference) in
+            if let error = error {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to save post to database: ", error)
+                return
+            }
+            print("Successfully saved post to database")
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
